4da7e346e0a205d3b1595848ebb04128f1717533
Abhishek-Kumar111 <ak7263036@gmail.com>
2024-10-21 21:19:41 +0530

    Add files via upload

---
+++ Obj9_website/js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function () {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
